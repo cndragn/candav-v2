40ecb1b07138233fa119c1fc723ad925126156f7
CNDavidson <cndragn@gmail.com>
2017-08-16 09:10:50 -0400

    Style pages and add icon to front page

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@ Template Name: Front Page
  get_header(); ?>
  <div class="container-fluid prelude">
  	<div class="row">
-		<div class="col-md-4 col-md-offset-7">
+		<div class="col-md-4 col-md-offset-7">		
 	 		<h1>Clean & Sleek Responsive Design</h1>
 	 			<p>Your web site should be a reflection of you, your product and/or services...that is what I strive to achieve.</p>
 
@@ -21,28 +21,31 @@ Template Name: Front Page
  		<div class="row container blocks">
  			<div class="col-md-4 info-square-outer">
  				<div class="info-square-inner">
+ 				<img src="<?php echo get_template_directory_uri(); ?>/images/003-coding.png">
  				<h2>Website Design</h2>
 					<p>From a single page website to a flexible CMS platform, you will get the right solution to fit your specific needs.</p>
 				</div>
  			</div>
  			<div class="col-md-4 info-square-outer">
  				<div class="info-square-inner">
- 				<h2>Website Design</h2>
-					<p>From a single page website to a flexible CMS platform, you will get the right solution to fit your specific needs.</p>
+ 				<img src="<?php echo get_template_directory_uri(); ?>/images/001-sky.png">
+ 				<h2>Web Hosting</h2>
+					<p>eliagble hosting and email along with your very own cPanel are available to you at an affordable rate.</p>
 				</div>
  			</div>
  			<div class="col-md-4 info-square-outer">
  				<div class="info-square-inner">
- 				<h2>Website Design</h2>
-					<p>From a single page website to a flexible CMS platform, you will get the right solution to fit your specific needs.</p>
+ 				<img src="<?php echo get_template_directory_uri(); ?>/images/002-settings.png">
+ 				<h2>Maintenance & Support</h2>
+					<p>Monthly packages include software updates, backups and web hosting as well as minor updates.</p>
 				</div>
  			</div>
  		</div>
  	</div>
  </div>
- <div class ="container action">
+ <div class ="container-fluid action">
  	<div class="row">
- 	<div class="col-md-9">
+ 	<div class="col-md-9 text-center">
  	<h3>Ready to get started?</h3>
  	<p>Get in touch with me today so that I may help find a solution to fit your business needs and budget.</p>
  	</div>
