37b2f57b8994ca6ad84c0daf330b8f2710a58e35
CNDavidson <cndragn@gmail.com>
2017-08-21 22:26:22 -0400

    setup master bulmapress for wp dev

--- candav-v1/js/customizer.js
+++ candav-v1/js/customizer.js
@@ -40,3 +40,9 @@
 		} );
 	} );
 } )( jQuery );
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
